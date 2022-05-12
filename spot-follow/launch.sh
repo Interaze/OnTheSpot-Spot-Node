c1d52b1ee95d5cdfc6ef83eb5dbd580f781b00e2
Stephen Bowen <62777692+sfbowen4@users.noreply.github.com>
2022-05-12 01:20:35 -0500

    fjlk

--- spot-follow/launch.sh
+++ spot-follow/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #python3 ./main.py --username ${SPOT_ACCOUNT} --password ${SPOT_PASSWORD} ${SPOT_IP}
-python3 /usr/src/app/spot-follow/replay_mission.py ${SPOT_IP} autowalk '/usr/src/app/Example Missions/summerlab.walk'
+python3 spot-follow/replay_mission.py ${SPOT_IP} autowalk 'Example Missions/summerlab.walk'
 # For commands that have to be run always at the start of the execution
 
 # Absolute File Paths (from source directory of your OS) /usr/home/app
