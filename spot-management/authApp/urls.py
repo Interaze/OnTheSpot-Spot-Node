9ba5d7287a417e0bc700e707697de644816876e2
cws3p4 <scullycphone@gmail.com>
2022-05-12 18:51:35 -0500

    j

--- spot-management/authApp/urls.py
+++ spot-management/authApp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from authApp.views import load_map, run_mission
+from authApp.views import run_mission
 
 urlpatterns = [
     #path('simple-upload/', simple_upload, name='Simple-Upload'),
