59c473dac83639636c72b9d6c821d22ca4b58273
Stephen Bowen <62777692+sfbowen4@users.noreply.github.com>
2022-05-12 01:18:04 -0500

    Added autowalk stuff

--- spot-follow/launch.sh
+++ spot-follow/launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 ./main.py --username ${SPOT_ACCOUNT} --password ${SPOT_PASSWORD} ${SPOT_IP}
+#python3 ./main.py --username ${SPOT_ACCOUNT} --password ${SPOT_PASSWORD} ${SPOT_IP}
+python3 /usr/src/app/spot-follow/replay_mission.py ${SPOT_IP} autowalk '/usr/src/app/Example Missions/summerlab.walk'
+
